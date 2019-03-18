167a602b7d62768729983ea409580ae6ece64fda
duvan baena <djbpuerta@hotmail.com>
2019-03-18 17:19:25 -0500

    Add product controller

---
+++ Sales/Sales.Backend/Models/localDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Sales.Backend.Models
+{
+    using Domain.Models;
+
+    public class LocalDataContext   : DataContext
+    {
+        public System.Data.Entity.DbSet<Sales.Common.Moldes.Product> Products { get; set; }
+    }
+}
---
+++ Sales/Sales.Common/Moldes/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Sales.Common.Moldes
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }        
+
+        [Required]
+        [StringLength(50)]
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public Decimal Price { get; set; }
+
+        [Display(Name = "Is Available")]
+        public bool IsAvailable { get; set; }
+
+        [Display(Name = "Publish On")]
+        [DataType(DataType.Date)]
+        public DateTime PublishOn { get; set; }
+    }
+}
