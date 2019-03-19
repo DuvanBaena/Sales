acd39e0d8338449f047e21fa4c5244bcd531e112
duvan baena <djbpuerta@hotmail.com>
2019-03-18 20:14:13 -0500

    Refresco del List View
    Refresco del List View

--- Sales/Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -3,13 +3,17 @@
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using Common.Moldes;
+    using GalaSoft.MvvmLight.Command;
     using Sales.Services;
     using Xamarin.Forms;
 
     public class ProductsViewModel  : BaseViewModel
     {
         private ApiService apiService;
+
+        private bool isRefreshing;
 
         private ObservableCollection<Product> products;
 
@@ -19,6 +23,13 @@
             set { this.SetValue(ref this.products, value); }
         }
 
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
+
+
         public ProductsViewModel()
         {
             this.apiService = new ApiService();
@@ -27,15 +38,31 @@
 
         private async void LoadProducts()
         {
-            var response = await this.apiService.GetList<Product>("https://projectsalesapi.azurewebsites.net","/api", "/Products");
+            this.IsRefreshing = true;
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+
+            var response = await this.apiService.GetList<Product>(url, "/api", "/Products");
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
 
             var list = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
+        }
+
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+
+            }
+           
         }
     }
 }
